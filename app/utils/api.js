f4416afc4accaf45e2c25505348fadc3edcab20d
ZongXu <Zongx98@gmail.com>
2017-09-24 04:14:55 -0400

    Handled request to Github API to query for player's repos and stars

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,60 @@
 var axios = require('axios');
 
+function getProfile (username) {
+  return axios.get('https://api.github.com/users/' + username).then((res) => {
+    return res.data;
+  });
+}
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users' + username + '/repos' + '&per_page=100').then((res) => {
+    return res;
+  });
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce((count, repo) => {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calcScore(profile, repos) {
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos);
+
+  return (followers * 5 + totalStars * 2);
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([getProfile(player), getRepos(player)]).then((data) => {
+    var profile = data[0];
+    var repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  });
+}
+
+function sortPlayers(players){
+  return players.sort((a,b) => {
+    return b.score - a.score;
+  });
+}
+
+
 module.exports = {
+
+  battle: (players) => {
+    return axios.all(players.map(getUserData)).then(sortPlayers).catch(handleError);
+  },
+
   fetchPopularRepos: (language) => {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'
       + language
